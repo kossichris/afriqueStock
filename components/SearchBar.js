dd4a6c045dd280ed3ec124b0797b28f187ea5ea5
Chrissmal <chrishouns21@gmail.com>
2022-02-19 11:17:54 +0100

    grid 1 search bar

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -45,7 +45,7 @@ const Searchbar = ({ page, hasFilter }) => {
                                 </div>
                                 <input type="text" placeholder="Rechercher des oeuvres créatives Photos Vecteurs Illustrations et Vidéos.." name="search" />
                                 <button type="submit" className="btn_1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="38" height="38" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#fff" fill="none" strokelinecap="round" strokelinejoin="round">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-search" width="38" height="38" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#fff" fill="none" strokelinecap="round" strokelinejoin="round">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                         <circle cx="10" cy="10" r="7" />
                                         <line x1="21" y1="21" x2="15" y2="15" />
@@ -55,7 +55,7 @@ const Searchbar = ({ page, hasFilter }) => {
                             </div>
                             <div className="col-lg-3 col-md-3 col-sm-3 col-xs-12 d-none d-sm-block ">
                                 <button type="submit" className="btn_2 d-flex flex-row justify-content-center">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-camera-rotate mt-2" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke={colors.primary} fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-camera-rotate mt-2" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke={colors.primary} fill="none" strokelinecap="round" strokelinejoin="round">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                         <path d="M5 7h1a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1a2 2 0 0 0 2 2h1a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2" />
                                         <path d="M11.245 15.904a3 3 0 0 0 3.755 -2.904m-2.25 -2.905a3 3 0 0 0 -3.75 2.905" />
