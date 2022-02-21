fad850433a890356949e5a51172890e26f042a63
Chrissmal <chrishouns21@gmail.com>
2022-02-21 20:01:57 +0100

    home retouchees

--- components/Footer.js
+++ components/Footer.js
@@ -32,7 +32,7 @@ const Footer = () => {
                                 <ul className="mt-3">
                                     <li><a className="rs mr-2" href="#0">
 
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" width="32" height="32" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#49B521" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-linkedin" width="32" height="32" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#49B521" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                             <rect x="4" y="4" width="16" height="16" rx="2" />
                                             <line x1="8" y1="11" x2="8" y2="16" />
