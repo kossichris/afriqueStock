c2916da0bd8a0f9e97c12afbf8876e3ad3156036
Chrissmal <chrishouns21@gmail.com>
2022-02-21 15:40:20 +0100

    home retouches

--- components/Footer.js
+++ components/Footer.js
@@ -181,7 +181,7 @@ const Footer = () => {
                         <ul id="footer-selector">
                             <li>
                                 <div className="select-dropdown mt-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-world" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#fff" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-world" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#fff" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                         <circle cx="12" cy="12" r="9" />
                                         <line x1="3.6" y1="9" x2="20.4" y2="9" />
